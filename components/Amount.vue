e7ca1e3d477d1050f488df9a59029208efbd5a2f
Psh <dev@mtbdx.com>
2021-07-25 18:05:52 +0900

    Amounts: Split big numbers to three-digit groups

--- components/Amount.vue
+++ components/Amount.vue
@@ -10,22 +10,37 @@ export default {
   props: {
     amount: { type: Number, required: true },
     currencyId: { type: Number, required: true },
+    color: { type: Boolean, default: true },
   },
   computed: {
     amountWhole() {
-      return Math.floor(
-        Math.abs(this.amount / Math.pow(10, this.currency.round))
-      )
+      const denominator = Math.pow(10, this.currency.round)
+      const number = Math.floor(Math.abs(this.amount) / denominator)
+
+      return number > 9999 ? this.splitByThree(number) : number
     },
     amountDec() {
-      const dec = Math.abs(this.amount % Math.pow(10, this.currency.round))
-      return dec.toString().padStart(this.currency.round, 0)
+      const denominator = Math.pow(10, this.currency.round)
+      const dec = Math.abs(this.amount) % denominator
+
+      return '.' + dec.toString().padStart(this.currency.round, 0)
     },
     currency() {
       return this.$store.state.globals.currencies[this.currencyId]
     },
     htmlClass() {
-      return this.amount > 0 ? 'positive' : 'negative'
+      return [
+        this.amount > 0 ? 'positive' : 'negative',
+        this.color ? 'color' : 'no-color',
+      ]
+    },
+  },
+  methods: {
+    splitByThree(number) {
+      return number
+        .toString()
+        .match(/(\d+?)(?=(\d{3})+(?!\d)|$)/g)
+        .join(' ')
     },
   },
 }
@@ -33,10 +48,10 @@ export default {
 
 <style lang="scss" scoped>
 .amount {
-  &.positive {
+  &.color.positive {
     color: green;
   }
-  &.negative {
+  &.color.negative {
     color: red;
   }
 
